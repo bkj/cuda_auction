27e3ddede0acef2859c86b85eff2aa91d7a187c1
bjohnson <bkj.322@gmail.com>
2018-09-18 20:47:08 -0400

    updating wrapper

--- wrapper.py
+++ wrapper.py
@@ -16,20 +16,23 @@ def __get_fn():
 
 __fn = __get_fn()
 
-def run_test(X):
+def run_test(X, verbose=False, num_runs=1, auction_max_eps=1.0, auction_min_eps=0.25, auction_factor=0.5):
+    
+    X_flat       = np.hstack(X).astype('float32')
     num_nodes    = int(X.shape[0])
     person2item  = np.zeros(num_nodes).astype('int32')
     
-    X_flat = np.hstack(X).astype('float32')
+    X_flat_p      = X_flat.ctypes.data_as(POINTER(c_float))
+    person2item_p = person2item.ctypes.data_as(POINTER(c_int))
     __fn(
-        X_flat.ctypes.data_as(POINTER(c_float)),
-        person2item.ctypes.data_as(POINTER(c_int)),
+        X_flat_p,
+        person2item_p,
         num_nodes,
-        int(1),   # verbose
-        int(2),   # num_runs
-        float(1.0), # auction_max_eps
-        float(1.0 / 4.0), # auction_min_eps
-        float(1.0 / 2.0), # auction_factor (0 means end immediately)
+        int(verbose),
+        int(num_runs),
+        float(auction_max_eps),
+        float(auction_min_eps),
+        float(auction_factor),
     )
     return person2item
 
@@ -38,18 +41,18 @@ np.random.seed(123)
 _ = run_test(np.random.choice(10, (10, 10)).astype('float32'))
 
 max_value = 100
-N         = 2 ** 12
+N         = 2 ** 13
 X = np.random.choice(max_value, (N, N)).astype('float32')
 
 t = time()
 jv_ass, _, _ = lapjv(X.max() - X)
 print('jv score', X[(np.arange(X.shape[0]), jv_ass)].sum(), file=sys.stderr)
-print('jv time', time() - t)
+print('jv time', int(1000 * (time() - t)))
 
 t = time()
-auc_ass = run_test(X)
+auc_ass = run_test(X, verbose=True)
 print('auction score', X[(auc_ass, np.arange(X.shape[0]))].sum(), file=sys.stderr)
-print('auction time', time() - t)
+print('auction time', int(1000 * (time() - t)))
 
 
 
